e6665ece82bcfbd4b0cb37231765e84e9ac0b554
Λ B H I N Λ V <ixd.abhi@gmail.com>
2021-10-15 20:12:38 +0530

    Head Tracking implemented plots for each and every value.
    Now I need to write some algorithms to detect walking  running, squating, slouching, drawsing etc.

--- Head-Tracking/app/src/main/java/com/black/xperiments/headtracking/MainActivity.kt
+++ Head-Tracking/app/src/main/java/com/black/xperiments/headtracking/MainActivity.kt
@@ -2,11 +2,13 @@ package com.black.xperiments.headtracking
 
 import android.Manifest
 import android.app.Dialog
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.Vibrator
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -21,6 +23,11 @@ import com.black.xperiments.headtracking.databinding.ActivityMainBinding
 import com.jins_jp.meme.MemeConnectListener
 import com.jins_jp.meme.MemeLib
 import com.jins_jp.meme.MemeRealtimeListener
+import com.jjoe64.graphview.DefaultLabelFormatter
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.GridLabelRenderer
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -32,7 +39,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val sensorViewModel: SensorViewModel by viewModels()
-
+    private val TAG = "SENSORVAL"
     /*Common variables*/
     private val handler = Handler(Looper.getMainLooper())
     private var memeLib: MemeLib? = null
@@ -43,6 +50,16 @@ class MainActivity : AppCompatActivity() {
     private var eogSensor = "not_found"
     private var eogPowerPrev = 0
     private var eogPowerProgressView: ProgressBar? = null
+
+    /* Plotting*/
+    private var seriesAccX = LineGraphSeries<DataPoint>()
+    private var seriesAccY = LineGraphSeries<DataPoint>()
+    private var seriesAccZ = LineGraphSeries<DataPoint>()
+    private var seriesPitch = LineGraphSeries<DataPoint>()
+    private var seriesYaw = LineGraphSeries<DataPoint>()
+    private var seriesRoll = LineGraphSeries<DataPoint>()
+
+    private var graphLastXValue = 5.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,14 +90,58 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             eogInit()
         }
-
+        initGraphs()
         observers()
+    }
+
+    private fun initGraphs(){
+        setSeries(seriesAccX, R.color.oneL, 3, R.color.one)
+        setSeries(seriesAccY, R.color.one, 3, R.color.one)
+        setSeries(seriesAccZ, R.color.twoL, 3, R.color.two)
+        setSeries(seriesPitch, R.color.two, 3, R.color.two)
+        setSeries(seriesYaw, R.color.threeL, 3, R.color.threeL)
+        setSeries(seriesRoll, R.color.three, 3, R.color.three)
+
+        binding.root.post {
+            binding.accXPlot.addSeries(seriesAccX)
+            binding.accYPlot.addSeries(seriesAccY)
+            binding.accZPlot.addSeries(seriesAccZ)
+            binding.pitchPlot.addSeries(seriesPitch)
+            binding.yawPlot.addSeries(seriesYaw)
+            binding.rollPlot.addSeries(seriesRoll)
+
+            setGraph(binding.accXPlot, 100, "Acc X")
+            setGraph(binding.accYPlot, 100, "Acc y")
+            setGraph(binding.accZPlot, 100, "Acc Z")
+            setGraph(binding.pitchPlot, 100, "Pitch")
+            setGraph(binding.yawPlot, 100, "Yaw")
+            setGraph(binding.rollPlot, 100, "Roll")
+        }
     }
 
     private fun observers(){
 
-    }
+        /* EOG */
+        sensorViewModel.getEOGConnect().observe(this, {
+            eogSensor = it
+            invalidateOptionsMenu()
+        })
 
+        sensorViewModel.getEOGPower().observe(this, {
+            eogPowerProgressView?.progress = it
+        })
+
+        sensorViewModel.getEOGAllData().observe(this,{
+            Log.d(TAG,"ACC-> aclX ${it.accX } aclY ${it.accY} aclZ ${it.accZ} GYRO -> P:${it.pitch} Y:${it.yaw} R:${it.roll}")
+            seriesAccX.appendData(DataPoint(graphLastXValue, it.accX.toDouble()), true, 100)
+            seriesAccY.appendData(DataPoint(graphLastXValue, it.accY.toDouble()), true, 100)
+            seriesAccZ.appendData(DataPoint(graphLastXValue, it.accZ.toDouble()), true, 100)
+            seriesPitch.appendData(DataPoint(graphLastXValue, it.pitch.toDouble()), true, 100)
+            seriesYaw.appendData(DataPoint(graphLastXValue, it.yaw.toDouble()), true, 100)
+            seriesRoll.appendData(DataPoint(graphLastXValue, it.roll.toDouble()), true, 100)
+            graphLastXValue += 1.0
+        })
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -233,5 +294,47 @@ class MainActivity : AppCompatActivity() {
     fun vibrate() {
         val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
         vibrator.vibrate(320L)
+    }
+
+    private fun setSeries(
+        series: LineGraphSeries<DataPoint>,
+        color: Int,
+        thickness: Int,
+        background: Int
+    ) {
+        val paint = Paint()
+        paint.isAntiAlias = true
+        paint.strokeWidth = thickness.toFloat()
+        paint.color = ContextCompat.getColor(this, color)
+        series.setCustomPaint(paint)
+        series.color = ContextCompat.getColor(this, color)
+        series.thickness = thickness
+//
+    }
+
+    private fun setGraph(graph: GraphView, max: Int, graphName: String) {
+        graph.title = graphName
+        graph.viewport.isXAxisBoundsManual = true
+        graph.viewport.setMinX(0.0)
+        graph.viewport.setMaxX(max.toDouble())
+        graph.viewport.isScalable = true
+//        graph.viewport.isYAxisBoundsManual = true
+//        graph.viewport.isXAxisBoundsManual = true
+        graph.gridLabelRenderer.gridStyle = GridLabelRenderer.GridStyle.HORIZONTAL
+        graph.gridLabelRenderer.labelFormatter = object : DefaultLabelFormatter() {
+            override fun formatLabel(value: Double, isValueX: Boolean): String {
+                return if (isValueX) {
+                    // show normal x values
+                    super.formatLabel(value, isValueX)
+                } else {
+                    // show currency for y values
+                    if (value > 1000) {
+                        super.formatLabel((value / 10000), isValueX) + " K"
+                    } else {
+                        super.formatLabel(value, isValueX)
+                    }
+                }
+            }
+        }
     }
 }
