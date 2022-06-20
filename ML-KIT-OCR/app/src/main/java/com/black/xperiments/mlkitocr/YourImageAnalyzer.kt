6456ab00b30fee4fdb0ee27300aaa28cabdcae7a
Λ B H I N Λ V <ixd.abhi@gmail.com>
2022-06-20 13:43:28 +0530

    MLKit based Text Recognition

---
+++ ML-KIT-OCR/app/src/main/java/com/black/xperiments/mlkitocr/YourImageAnalyzer.kt
@@ -0,0 +1,15 @@
+package com.black.xperiments.mlkitocr
+
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+
+class YourImageAnalyzer : ImageAnalysis.Analyzer {
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+        }
+    }
+}
